20df0e085251c51a8d9832bd52d7f7b5db5e5c0a
chisan01 <chisan2001@gmail.com>
2022-11-06 18:24:23 +0900

    add 책 추가하는 기능

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,16 @@
 import java.sql.DriverManager
 
 fun main(args: Array<String>) {
+    val allBook = getAllBook()
+    allBook.forEach(::println)
+
+    addBook(Book(
+        bookId = allBook.last().bookId + 1,
+        bookName = "test",
+        publisher = "chisan",
+        price = 10000
+    ))
+
     getAllBook().forEach(::println)
 }
 
@@ -13,7 +23,7 @@ fun getAllBook(): List<Book> {
             "chisanahn", "1234"
         )
         val stmt = con.createStatement()
-        val rs = stmt.executeQuery("SELECT * FROM Book")
+        val rs = stmt.executeQuery("SELECT * FROM Book ORDER BY bookid")
         while (rs.next()) {
             result.add(Book(
                 bookId = rs.getInt(1),
@@ -27,4 +37,24 @@ fun getAllBook(): List<Book> {
         e.printStackTrace()
     }
     return result
+}
+
+fun addBook(book: Book) {
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver")
+        val con = DriverManager.getConnection(
+            "jdbc:mysql://192.168.55.189:4567/madang",
+            "chisanahn", "1234"
+        )
+        val pstmt = con.prepareStatement("INSERT INTO Book(bookid, bookname, publisher, price) values(?, ?, ?, ?)")
+        pstmt.setInt(1, book.bookId)
+        pstmt.setString(2, book.bookName)
+        pstmt.setString(3, book.publisher)
+        book.price?.let { pstmt.setInt(4, it) }
+
+        pstmt.executeUpdate()
+        con.close()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
